e568c52bdb760299501ebca4fae845767bba6af9
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-04-18 01:48:02 +0700

    Add project files.

---
+++ SMDBB_TabelAdmin/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace SMDBB_TabelAdmin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtIDAdmin = new System.Windows.Forms.TextBox();
+            this.txtNamaAdmin = new System.Windows.Forms.TextBox();
+            this.txtPasswd = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dgvAdmin = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAdmin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID Admin";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nama Admin";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password";
+            // 
+            // txtIDAdmin
+            // 
+            this.txtIDAdmin.Location = new System.Drawing.Point(168, 43);
+            this.txtIDAdmin.Name = "txtIDAdmin";
+            this.txtIDAdmin.Size = new System.Drawing.Size(159, 22);
+            this.txtIDAdmin.TabIndex = 3;
+            // 
+            // txtNamaAdmin
+            // 
+            this.txtNamaAdmin.Location = new System.Drawing.Point(168, 135);
+            this.txtNamaAdmin.Name = "txtNamaAdmin";
+            this.txtNamaAdmin.Size = new System.Drawing.Size(159, 22);
+            this.txtNamaAdmin.TabIndex = 4;
+            // 
+            // txtPasswd
+            // 
+            this.txtPasswd.Location = new System.Drawing.Point(168, 220);
+            this.txtPasswd.Name = "txtPasswd";
+            this.txtPasswd.Size = new System.Drawing.Size(159, 22);
+            this.txtPasswd.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(501, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Tambah";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonTambah);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(665, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Hapus";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.buttonHapus);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(501, 166);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(665, 166);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Refresh";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.buttonRefresh);
+            // 
+            // dgvAdmin
+            // 
+            this.dgvAdmin.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAdmin.Location = new System.Drawing.Point(12, 267);
+            this.dgvAdmin.Name = "dgvAdmin";
+            this.dgvAdmin.RowHeadersWidth = 51;
+            this.dgvAdmin.RowTemplate.Height = 24;
+            this.dgvAdmin.Size = new System.Drawing.Size(844, 216);
+            this.dgvAdmin.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 495);
+            this.Controls.Add(this.dgvAdmin);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPasswd);
+            this.Controls.Add(this.txtNamaAdmin);
+            this.Controls.Add(this.txtIDAdmin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAdmin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtIDAdmin;
+        private System.Windows.Forms.TextBox txtNamaAdmin;
+        private System.Windows.Forms.TextBox txtPasswd;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dgvAdmin;
+    }
+}
+
---
+++ SMDBB_TabelAdmin/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SMDBB_TabelAdmin
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "Data Source=LAPTOP-97JFDRMG\\AZIZAH_A_AZMI;Initial Catalog=DonasiBarangBekas;user id = sa; password=@@SuguruMyLove777";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void ClearForm()
+        {
+            txtIDAdmin.Clear();
+            txtNamaAdmin.Clear();
+            txtPasswd.Clear();
+
+            txtIDAdmin.Focus();
+        }
+
+        private void LoadData()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "SELECT ID_Admin AS [ID Admin], Nama_Admin AS [Nama Admin], Passwd AS [Password] FROM Admin";
+                    SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    dgvAdmin.AutoGenerateColumns = true;
+                    dgvAdmin.DataSource = dt;
+
+                    ClearForm(); // Kosongkan inputan
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void buttonTambah(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    if (txtIDAdmin.Text == "" || txtNamaAdmin.Text == "" || txtPasswd.Text == "")
+                    {
+                        MessageBox.Show("Harap isi semua data", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    conn.Open();
+                    string query = "INSERT INTO Admin (ID_Admin, Nama_Admin, Passwd) VALUES (@ID_Admin, @Nama_Admin, @Passwd)";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+
+                    cmd.Parameters.AddWithValue("@ID_Admin", txtIDAdmin.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Nama_Admin", txtNamaAdmin.Text.Trim());
+                    cmd.Parameters.AddWithValue("@Passwd", txtPasswd.Text.Trim());
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Data berhasil ditambahkan!", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        LoadData();
+                        ClearForm();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Data tidak berhasil ditambahkan!", "Gagal", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void buttonHapus(object sender, EventArgs e)
+        {
+            if (dgvAdmin.SelectedRows.Count > 0)
+            {
+                DialogResult confirm = MessageBox.Show("Yakin ingin menghapus data ini?", "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (confirm == DialogResult.Yes)
+                {
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        try
+                        {
+                            string id = dgvAdmin.SelectedRows[0].Cells["ID_Admin"].Value.ToString();
+                            string query = "DELETE FROM Admin WHERE ID_Admin = @ID_Admin";
+
+                            using (SqlCommand cmd = new SqlCommand(query, conn))
+                            {
+                                conn.Open();
+                                cmd.Parameters.AddWithValue("@ID_Admin", id);
+                                int rowsAffected = cmd.ExecuteNonQuery();
+
+                                if (rowsAffected > 0)
+                                {
+                                    MessageBox.Show("Data berhasil dihapus!", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                    ClearForm(); // Kosongkan form
+                                    LoadData();  // Tampilkan data terbaru
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Data tidak ditemukan atau gagal dihapus!", "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error: " + ex.Message, "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Pilih data yang akan dihapus!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void buttonRefresh(object sender, EventArgs e)
+        {
+            LoadData();
+
+            MessageBox.Show($"Jumlah Kolom: {dgvAdmin.ColumnCount}\nJumlah Baris: {dgvAdmin.RowCount}",
+                "Debugging DataGridView", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+    }
+}
